92b466807cbe2a5cfd5a7dd9bf8890d093f190d5
Kireenko Olga <kireenko86.osil@mail.ru>
2021-12-21 10:31:27 +0500

    add test

---
+++ src/test/java/ru/netology/selenium/shouldTest.java
@@ -0,0 +1,62 @@
+package ru.netology.selenium;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class shouldTest {
+
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setupAll() {
+
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setup() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+
+    }
+
+    @Test
+    public void shouldTest() {
+        driver.get("http://localhost:9999");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Василий");
+        elements.get(1).sendKeys("+79270000000");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.className("paragraph")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
+
+    @Test
+    public void shouldFindByCSSSelectorTest() {
+        driver.get("http://localhost:9999");
+        //WebElement form = driver.findElement(By.cssSelector("[data-test-id=order-form]"));
+        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Василий");
+        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79270000000");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("[type='button']")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id ='order-success']")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+
+    }
+}
+
